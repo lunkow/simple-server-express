0e6e5b60c7ecb39ed1a3ef2f8eb23afcf9ee2fce
Anton Lunkov <lunkow@live.ru>
2023-05-18 01:59:14 +0300

    Add express with hbs

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const http = require('http');
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
 const port = 3000;
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
-};
-const server = http.createServer(requestHandler);
 
-server.listen(port, (err) => {
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (request, response) => {
+    response.render('home', {
+        name: 'John'
+    });
+})
+
+app.use((err, request, response, next) => {
+    // логирование ошибки, пока просто console.log
+    console.log(err)
+    response.status(500).send('Something broke!')
+})
+
+app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }    console.log(`server is listening on ${port}`)
